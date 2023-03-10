c45bc6e5d1a205314760d70dfefeace143a44791
naveenm3051 <naveenm3051@gmail.com>
2023-03-10 15:26:51 +0530

    Done

--- Stock.cpp
+++ Stock.cpp
@@ -1,24 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int maxpr(vector<int> &arr)
+vector<int> maxpr(vector<int> &arr)
 {
+    vector<int> ans;
     int currentp = arr[0];
+    int buy = 0;
+    int sell = 0;
     int maxp = INT_MIN;
     for (int i = 1; i < arr.size(); i++)
     {
         if (arr[i] - currentp > maxp)
         {
             maxp = arr[i] - currentp;
-            cout << "Max P-->" << maxp << endl;
+            // cout << "Max P-->" << maxp << endl;
+            sell = i;
         }
         else
         {
             currentp = arr[i];
-            cout << "Current P-->" << currentp << endl;
+            buy = i;
+            // cout << "Current P-->" << currentp << endl;
         }
     }
-    return maxp;
+    ans.push_back(maxp);
+    ans.push_back(buy);
+    ans.push_back(sell);
+    return ans;
 }
 int main()
 {
@@ -31,7 +39,7 @@ int main()
         cin >> t;
         arr.push_back(t);
     }
-    int maxp = maxpr(arr);
-    cout << maxp;
+    vector<int> maxp = maxpr(arr);
+    cout << "Maximum Profit--> " << maxp[0] << " Bought at Day ->" << maxp[1] << " Sold at Day ->" << maxp[2];
     return 0;
 }
